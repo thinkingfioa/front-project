e7db2815149c1f8b262330752acb5c3e35513d6d
thinking_fioa <thinking_fioa@163.com>
2018-04-06 16:39:51 +0800

    changeInfo

---
+++ changeInfo/data.js
@@ -0,0 +1,243 @@
+var data = [
+    {
+        'name': '前端',
+        'list':[
+            {
+                'title': '前端工程师',
+                'company': '北京信息科技有限公司',
+                'position':'前端页面',
+                'recruitingNumbers':'2人',
+                'workingLocation':'北京',
+                'workExperience':'不限',
+                'education':'不限',
+                'wage':'面议',
+                'addDate': '2018-04-06'
+            },
+            {
+                'title': '网站前端页面设计',
+                'company': '上海信息科技有限公司',
+                'position':'前端页面',
+                'recruitingNumbers':'12人',
+                'workingLocation':'上海',
+                'workExperience':'不限',
+                'education':'不限',
+                'wage':'面议',
+                'addDate': '2018-04-06'
+            },
+            {
+                'title': '高级web前端工程师',
+                'company': '广州信息科技有限公司',
+                'position':'前端页面',
+                'recruitingNumbers':'10人',
+                'workingLocation':'广州',
+                'workExperience':'3年',
+                'education':'本科',
+                'wage':'面议',
+                'addDate': '2018-04-06'
+            },
+            {
+                'title': 'HTML5前端工程师',
+                'company': '南京信息科技有限公司',
+                'position':'前端页面',
+                'recruitingNumbers':'1人',
+                'workingLocation':'南京',
+                'workExperience':'不限',
+                'education':'不限',
+                'wage':'面议',
+                'addDate': '2018-04-06'
+            },
+            {
+                'title': 'JavaScript开发',
+                'company': '湖北信息科技有限公司',
+                'position':'前端页面',
+                'recruitingNumbers':'10人',
+                'workingLocation':'湖北',
+                'workExperience':'不限',
+                'education':'不限',
+                'wage':'面议',
+                'addDate': '2018-04-06'
+            },
+            {
+                'title': '页面前端架构设计',
+                'company': '武汉信息科技有限公司',
+                'position':'前端页面',
+                'recruitingNumbers':'2人',
+                'workingLocation':'武汉',
+                'workExperience':'不限',
+                'education':'不限',
+                'wage':'面议',
+                'addDate': '2018-04-06'
+            },
+            {
+                'title': '前端测试工程师',
+                'company': '广州信息科技有限公司',
+                'position':'前端页面',
+                'recruitingNumbers':'3人',
+                'workingLocation':'广州',
+                'workExperience':'不限',
+                'education':'不限',
+                'wage':'面议',
+                'addDate': '2018-04-06'
+            },
+            {
+                'title': 'Bootstrap3项目维护与开发工程师',
+                'company': '成都信息科技有限公司',
+                'position':'前端页面',
+                'recruitingNumbers':'2人',
+                'workingLocation':'成都',
+                'workExperience':'不限',
+                'education':'不限',
+                'wage':'面议',
+                'addDate': '2018-04-06'
+            },
+            {
+                'title': 'UI设计',
+                'company': '杭州信息科技有限公司',
+                'position':'前端页面',
+                'recruitingNumbers':'4人',
+                'workingLocation':'杭州',
+                'workExperience':'不限',
+                'education':'不限',
+                'wage':'面议',
+                'addDate': '2018-04-06'
+            },
+            {
+                'title': 'HTML5前端工程师',
+                'company': '北京云计算有限公司',
+                'position':'前端页面',
+                'recruitingNumbers':'1人',
+                'workingLocation':'北京',
+                'workExperience':'不限',
+                'education':'不限',
+                'wage':'面议',
+                'addDate': '2018-04-06'
+            },
+            {
+                'title': '前端页面开发',
+                'company': '天津科技有限公司',
+                'position':'前端页面',
+                'recruitingNumbers':'9人',
+                'workingLocation':'天津',
+                'workExperience':'不限',
+                'education':'不限',
+                'wage':'面议',
+                'addDate': '2018-04-06'
+            },
+            {
+                'title': '前端项目结构设计与调优',
+                'company': '新疆科技有限公司',
+                'position':'前端页面',
+                'recruitingNumbers':'7人',
+                'workingLocation':'新疆',
+                'workExperience':'不限',
+                'education':'不限',
+                'wage':'面议',
+                'addDate': '2018-04-06'
+            },
+            {
+                'title': '前端工程师',
+                'company': '合肥信息科技有限公司',
+                'position':'前端页面',
+                'recruitingNumbers':'3人',
+                'workingLocation':'合肥',
+                'workExperience':'不限',
+                'education':'不限',
+                'wage':'面议',
+                'addDate': '2018-04-06'
+            },
+            {
+                'title': 'Web工程师',
+                'company': '杭州谐云科技有限公司',
+                'position':'前端页面',
+                'recruitingNumbers':'6人',
+                'workingLocation':'杭州',
+                'workExperience':'不限',
+                'education':'不限',
+                'wage':'面议',
+                'addDate': '2018-04-06'
+            },
+            {
+                'title': 'Freemarker项目前端工程师',
+                'company': '菜鸟有限公司',
+                'position':'前端页面',
+                'recruitingNumbers':'23人',
+                'workingLocation':'杭州',
+                'workExperience':'不限',
+                'education':'不限',
+                'wage':'面议',
+                'addDate': '2018-04-06'
+            }
+        ]
+    },
+    {
+        'name': '后端',
+        'list': [
+            {
+                'title'	:	'PHP工程师',
+                'company'	:	'杭州xxxx有限公司 ',
+                'position'	:	'PHP后台',
+                'recruitingNumbers'	:	'2人',
+                'workingLocation'	:	'杭州',
+                'workExperience'	:	'不限',
+                'education'	:	'不限',
+                'wage'	:	'面议',
+                'addDate'	:	'2018-4-05'
+            },
+            {
+                'title'	:	'诚招java开发工程师',
+                'company'	:	'广州xxxx中心 ',
+                'position'	:	'java开发',
+                'recruitingNumbers'	:	'2人',
+                'workingLocation'	:	'广州',
+                'workExperience'	:	'不限',
+                'education'	:	'不限',
+                'wage'	:	'面议',
+                'addDate'	:	'2018-4-05'
+            },
+            {
+                'title'	:	'java工程师',
+                'company'	:	'深圳xxxx科技有限公司 ',
+                'position'	:	'java后台',
+                'recruitingNumbers'	:	'2人',
+                'workingLocation'	:	'深圳',
+                'workExperience'	:	'不限',
+                'education'	:	'不限',
+                'wage'	:	'面议',
+                'addDate'	:	'2018-4-05'
+            },
+            {
+                'title'	:	'.net工程师',
+                'company'	:	'北京xxxx网络科技有限公司 ',
+                'position'	:	'.net程序员',
+                'recruitingNumbers'	:	'3人',
+                'workingLocation'	:	'通州',
+                'workExperience'	:	'不限',
+                'education'	:	'不限',
+                'wage'	:	'面议',
+                'addDate'	:	'2018-4-05'
+            },
+            {
+                'title'	:	'招聘php程序员',
+                'company'	:	'xxxx教育科技有限公司 ',
+                'position'	:	'php程序员',
+                'recruitingNumbers'	:	'2人',
+                'workingLocation'	:	'北京',
+                'workExperience'	:	'不限',
+                'education'	:	'不限',
+                'wage'	:	'面议',
+                'addDate'	:	'2018-4-05'
+            },
+            {
+                'title'	:	'PHP高级工程师',
+                'company'	:	'北京xxxx管理有限公司 ',
+                'position'	:	'PHP高级工程师',
+                'recruitingNumbers'	:	'4人',
+                'workingLocation'	:	'北京',
+                'workExperience'	:	'不限',
+                'education'	:	'不限',
+                'wage'	:	'面议',
+                'addDate'	:	'2018-4-05'
+            }
+        ]
+    }
+]
